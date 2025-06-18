a5134a2607212efad1a186a37a57a02b568256bc
Sandenberg Ferreira Melo <77549672+sandenbergmelo@users.noreply.github.com>
2025-06-18 17:14:08 -0300

    Add eslint 'import-newlines' and update dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,19 @@
 import baseConfig from '@rocketseat/eslint-config/node.mjs'
+import importNewLines from 'eslint-plugin-import-newlines'
 import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
-  ...baseConfig,
   {
     ignores: ['src/discord/base/**', 'src/settings/**'],
   },
   {
+    extends: [...baseConfig],
+    plugins: {
+      'import-newlines': importNewLines,
+    },
+    rules: {
+      'import-newlines/enforce': ['warn', { items: 40, 'max-len': 80 }],
+    },
     languageOptions: {
       globals: {
         animated: 'readonly',
